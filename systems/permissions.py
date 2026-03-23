f6bb501f8282847c03ce0ebcf7754fe073900334
terebravisse <gnarishtnt@gmail.com>
2026-03-23 18:48:54 +0200

    reformed permissions

--- systems/permissions.py
+++ systems/permissions.py
@@ -8,22 +8,25 @@ def is_sinner_anywhere(user_id):
     return cursor.fetchone() is not None
 
 def is_authorized(ctx):
-    if is_sinner_anywhere(ctx.author.id):
-        return False
-
-    # server owner always allowed
+    # Server owner is ALWAYS authorized to use admin commands, even if they are a sinner somewhere else.
     if ctx.guild.owner_id == ctx.author.id:
         return True
+
+    # If not the owner, check if they are a sinner anywhere
+    if is_sinner_anywhere(ctx.author.id):
+        return False
 
     role = discord.utils.get(ctx.author.roles, name=PUNISHER_ROLE_NAME)
     return role is not None
 
 def is_admin(user, guild):
-    if is_sinner_anywhere(user.id):
-        return False
-
+    # Server owner is ALWAYS an admin
     if guild.owner_id == user.id:
         return True
+        
+    # If not the owner, check if they are a sinner anywhere
+    if is_sinner_anywhere(user.id):
+        return False
     
     role = discord.utils.get(user.roles, name=PUNISHER_ROLE_NAME)
     return role is not None
